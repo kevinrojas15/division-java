be94a0d0a11552640e5a781fc95ec001001e6a47
Juan Espinosa <x@gmail.com>
2024-08-01 19:39:39 -0500

    pp
    version1.0

---
+++ division1/src/main/java/com/mycompany/division1/Division1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.division1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Division1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
